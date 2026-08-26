f917bb3d32776bffe92154c8665267741fc07f2f
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-08-25 20:14:22 -0500

    Add files via upload

---
+++ practice/2 - Introduction to R - Part 1.R
@@ -0,0 +1,36 @@
+
+##################################
+### Introduction to R - Part 1 ###
+##################################
+
+
+# 1. Load tidyverse package.
+
+
+
+
+# 2. Read in dataset heart_study.
+
+
+
+
+# 3. Extract the names of the columns of the
+#    heart_study dataset.
+
+
+
+
+# 4. For the variables: 
+#    systolic blood pressure,
+#    cigarettes per day, 
+#    body mass index, 
+#    heart rate, and
+#    glucose level
+#    Compute the following summary statistics:
+#    1. Count the number of subjects
+#    2. Mean
+#    3. Standard Deviation
+#    4. Minimum
+#    5. Median 
+#    6. Maximum
+
