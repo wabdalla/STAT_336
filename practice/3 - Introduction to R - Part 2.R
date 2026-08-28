6512a7dd062ff35e1f9504b011d1d42258b0b042
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-08-27 22:55:32 -0500

    Add files via upload

---
+++ practice/3 - Introduction to R - Part 2.R
@@ -0,0 +1,50 @@
+##################################
+### Introduction to R - Part 2 ###
+##################################
+
+
+# 1. load tidyverse package. 
+
+
+
+# 2. Read in dataset heart_study.
+
+
+# 3. For the variables: 
+#    systolic blood pressure,
+#    cigarettes per day, 
+#    body mass index, 
+#    heart rate, and
+#    glucose level
+#    Compute the following summary statistics 
+#    by Gender:
+#    1. Count the number of subjects
+#    2. Mean
+#    3. Standard Deviation
+#    4. Minimum
+#    5. Median 
+#    6. Maximum
+
+
+
+# 4. For the variables: 
+#    systolic blood pressure,
+#    cigarettes per day, 
+#    body mass index, 
+#    heart rate, and
+#    glucose level
+#    Compute the following summary statistics 
+#    by age group:
+#    1. Count the number of subjects
+#    2. Mean
+#    3. Standard Deviation
+#    4. Minimum
+#    5. Median 
+#    6. Maximum
+
+
+
+# 5. Try this analysis with other variables from 
+#    the heart study data. 
+
+
