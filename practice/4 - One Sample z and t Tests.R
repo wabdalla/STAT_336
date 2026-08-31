7f147032e84830d0cd6cae07f1ed6633b234a2ba
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-08-30 22:14:13 -0500

    Add files via upload

---
+++ practice/4 - One Sample z and t Tests.R
@@ -0,0 +1,14 @@
+
+################################
+### One Sample z and t Tests ###
+################################
+
+
+# 1. load tidyverse package and read the dataset 
+#    called 4_one_sample_z_and_t_tests.
+
+
+
+
+# 2. Perform a one sample t.test on the column HC.  
+
