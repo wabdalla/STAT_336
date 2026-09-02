2c8e8a7b11ee8ca49f9c05619423726a4acbdd13
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-09-01 23:04:30 -0500

    Add files via upload

---
+++ practice/5 - independent sample t test.R
@@ -0,0 +1,16 @@
+
+###################################
+### Independent Samples t-Tests ### 
+###################################
+
+
+# 1. load tidyverse package and read the dataset 
+#    called 5_independent_sample_t_test.
+
+
+
+# 2. Perform a two sample t.test. Group 1 is the Experimental and Group 2 is the Control.
+
+
+
+
