4f3cbfdacd5ffa9479f150c0fc47cc1d6a8d30e4
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-09-03 21:01:08 -0500

    Add files via upload

---
+++ practice/6 - paired-sample t test.R
@@ -0,0 +1,18 @@
+
+##############################
+### Paired Samples t-Tests ### 
+##############################
+
+
+# 1. load tidyverse package and read the dataset 
+#    called 6_paired_sample_t_test_frog.
+
+
+
+
+# 2. Perform a paired sample t.test. 
+#    Group 1 is the Control and Group 2 is the Progesterone.
+
+
+
+
