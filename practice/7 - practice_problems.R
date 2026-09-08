641e777425baab554964dd9111d9056453a891db
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-09-08 18:49:33 -0500

    Add files via upload

---
+++ practice/7 - practice_problems.R
@@ -0,0 +1,37 @@
+
+#########################
+### Practice Problems ### 
+#########################
+
+
+# 1. load tidyverse package and read the dataset 
+#    called 7_exercise_1_rats. Perform the corresponding 
+#    test that matches your output.
+
+
+
+
+
+# 2. Read the dataset called 7_exercise_2_salamanders. 
+#    Perform the corresponding that matches your output.
+
+
+
+
+
+
+
+# 3. Read the dataset called 7_exercise_3_lizards. 
+#    Perform the corresponding test that matches your output.
+
+
+
+
+
+# 4. Read the dataset called 7_exercise_4_pool_and_riffle. 
+#    Perform the corresponding test that matches your output.
+
+
+
+
+
