835f4dbfb9053478f93eba743d8b9b5a0718f3e6
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-09-10 23:50:32 -0500

    Add files via upload

---
+++ practice/8 - one_way_anova_stress.R
@@ -0,0 +1,15 @@
+
+####################################
+########## One-Way ANOVA ###########
+### Completely Randomized Design ###
+####################################
+
+
+# 1. load tidyverse package and read the dataset 
+#    called 8_one_way_anova_stress.
+
+
+
+# 2. Run a one-way ANOVA on the data. 
+
+
