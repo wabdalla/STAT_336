e3c6501ef989754ba3b96fbdad318e5ceef31464
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-09-13 23:20:45 -0500

    Add files via upload

---
+++ practice/9 - one_way_anova_corn.R
@@ -0,0 +1,19 @@
+
+####################################
+########## One-Way ANOVA ###########
+### Completely Randomized Design ###
+###              &              ###
+#### Quasi-Experimental Design #####
+####################################
+
+
+# 1. load tidyverse package and read the dataset 
+#    called 9_one_way_anova_corn.
+
+
+
+
+# 2. Run a one-way ANOVA on the data. 
+
+
+
