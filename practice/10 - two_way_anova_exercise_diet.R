a9f84e58909be6698277cec109b978742c6fe595
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-09-16 03:16:29 -0500

    Add files via upload

---
+++ practice/10 - two_way_anova_exercise_diet.R
@@ -0,0 +1,19 @@
+
+#########################################
+############# TWO-Way ANOVA #############
+### Factorial Design with Two Factors ###
+#########################################
+
+
+# 1. load tidyverse package and read the dataset 
+#    called 10_two_way_anova_exercise_diet. 
+
+
+
+
+
+# 2. Perform the 2-way ANOVA test to match your output.
+
+
+
+
