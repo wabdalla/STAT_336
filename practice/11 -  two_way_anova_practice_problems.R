6082ecf10a422f430ab164f4f6a7966c0bcff71d
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-09-17 20:46:39 -0500

    Add files via upload

---
+++ practice/11 -  two_way_anova_practice_problems.R
@@ -0,0 +1,18 @@
+
+#########################
+###### 2-Way ANOVA ######
+### Practice Problems ### 
+#########################
+
+
+# 1. load tidyverse package and read the dataset 
+#    called 11_exercise_2_leaf_areas. 
+
+
+
+
+
+# 2. Perform the 2-way ANOVA test to match your output.
+
+
+
