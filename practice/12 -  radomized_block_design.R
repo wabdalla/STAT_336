03616ff99cc5e984933f8cf4f025e111d1a29e5d
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-09-21 00:26:06 -0500

    Add files via upload

---
+++ practice/12 -  radomized_block_design.R
@@ -0,0 +1,20 @@
+
+#############################################
+########## Randomized Block ANOVA ###########
+#############################################
+
+
+# 1. load tidyverse package and read the dataset 
+#    called 12_exercise_1_alfalfa_plants.
+
+
+
+
+# 2. Run a randomized block ANOVA on the data.
+
+
+
+
+# 3. Run a one-way ANOVA on the data. 
+
+
