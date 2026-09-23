a9717148cf3c900e0698eaa63a4d036b2180d8ec
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-09-22 19:27:59 -0500

    Add files via upload

---
+++ practice/13 -  reapeated_measures_design.R
@@ -0,0 +1,29 @@
+##############################################
+########## Randomized Block Design ###########
+######### Repeated Measures Design ###########
+##############################################
+
+
+# 1. load tidyverse package and read the dataset 
+#    called 13_exercise_1_nurses.
+
+
+
+# 2. Run a randomized block ANOVA on the data for exercise 1
+#    and the incorrect One-way ANOVA.
+
+
+
+
+# 3. load tidyverse package and read the dataset 
+#    called 13_exercise_2_snakes.
+
+
+
+# 4. Run a randomized block ANOVA on the data for exercise 2
+#    and the incorrect one-way ANOVA.
+
+
+
+
+
