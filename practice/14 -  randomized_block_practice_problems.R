dfe11a3b9cf1453dc4c783ca68fd67908b262cc9
Widad Abdalla <156271579+wabdalla@users.noreply.github.com>
2026-09-24 18:49:16 -0500

    Add files via upload

---
+++ practice/14 -  randomized_block_practice_problems.R
@@ -0,0 +1,24 @@
+
+##############################################
+########## Randomized Block Design ###########
+######### Repeated Measures Design ###########
+############# Practice Problems ##############
+##############################################
+
+
+# 1. load tidyverse package and read the dataset 
+#    called 14_exercise_1_barley.
+
+
+
+
+# 2. Run a the codes for the corresponding ANOVA on the data.
+
+
+
+
+
+
+# 3. Run a one-way ANOVA on the data. This is an incorrect design. 
+
+
